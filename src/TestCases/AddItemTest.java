f20c93c9114ebc72f3e6258e125caf3e5349bc41
shakersaba <shakersaba@cmail.carleton.ca>
2017-10-07 17:24:16 -0400

    Add AddItemTest

---
+++ src/TestCases/AddItemTest.java
@@ -0,0 +1,19 @@
+package TestCases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddItemTest {
+
+	@Test
+	public void test() throws Exception {
+		//setup
+		try{
+			fail("Not yet implemented");
+		} catch (Exception e) {
+			fail("Not yet implemented");
+		}
+	}
+
+}
