74600f87524d276cf99d8f846b367a3b45d4e32c
shakersaba <shakersaba@cmail.carleton.ca>
2017-10-08 20:35:27 -0400

    Borrow Loan Copy Test 1st run
    incomplete test

--- src/test_cases/BorrowLoancopyTest.java
+++ src/test_cases/BorrowLoancopyTest.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-//import server.logic.handler.OutputHandler;
+import server.logic.handler.OutputHandler;
+import server.logic.tables.ItemTable;
+import server.logic.tables.TitleTable;
 
 public class BorrowLoancopyTest {
 
@@ -12,7 +14,21 @@ public class BorrowLoancopyTest {
 	public void test() throws Exception {
 		//setup
 		try{
-			fail("Not yet implemented");
+			OutputHandler outHan = new OutputHandler();
+			
+			outHan.createTitle("1234567890000,Test Title 1");
+			for(int i=0;i<20;++i)
+				outHan.createItem("1234567890000");
+			outHan.createUser("test@username1,testpassword");
+			outHan.createUser("test@username2,testpassword");
+			//assertEquals("Borrow books.","Success!",outHan.borrow("test@username1,1234567890000,1").getOutput());
+			//assertEquals("Failure due to user not existing.","The User Does Not Exist!",outHan.borrow("nonexistant@username,1234567890000,1").getOutput());
+			for(int i=0;i<2;++i)
+				assertEquals("Borrow books.","Success!",outHan.borrow("test@username1,1234567890000,1").getOutput());
+			//assertEquals("Failure due to missing item.","The Item is Not Available!",outHan.borrow("test@username2,1234567890000,1").getOutput());
+			//assertEquals("Failure due to reaching borrow limit.","The Maximun Number of Items is Reached!",outHan.borrow("test@username1,1234567890000,1").getOutput());
+			//outHan.collectFine("test@username2,2000000");
+			//assertEquals("Failure due to outstanding fees.","Outstanding Fee Exists!",outHan.borrow("test@username2,1234567890000,1").getOutput());
 		} catch (Exception e) {
 			fail("Exception occured");
 		} finally {
